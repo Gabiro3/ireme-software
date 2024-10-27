9b3d371383787b683484c64c668d35ef6c9d15e5
Gabiro3 Arnaud <gabiroarnaud@gmail.com>
2024-10-27 17:12:22 +0200

    Version 1.1 deploy. Features: email invoicing, data filtering, user onboarding and company settings

--- src/modules/AuthModule/index.jsx
+++ src/modules/AuthModule/index.jsx
@@ -5,7 +5,7 @@ import { Layout, Col, Divider, Typography } from 'antd';
 import AuthLayout from '@/layout/AuthLayout';
 import SideContent from './SideContent';
 
-import logo from '@/style/images/ireme logo.svg';
+import logo from '@/style/images/Ireme logo.svg';
 
 const { Content } = Layout;
 const { Title } = Typography;
